872116122b4bc288b50a9b078a55363d5b3bbd90
Jayde 3 <93054701+BinaryCrown@users.noreply.github.com>
2023-04-10 13:24:58 +0100

    Added j0 search in collapsing worm.

--- collapsing.py
+++ collapsing.py
@@ -1,7 +1,19 @@
+x = lambda a,b: (a[0]<b[0]) or (a[0]==b[0] and x(a[1:],b[1:])) if len(a)>0 and len(b)>0 else ((a==[] or b==[]) and len(a)<len(b))
+
 def BR(a):
     br = None
     for i in range(len(a)):
         if a[len(a)-1-i] < a[-1]:
+            br=len(a)-i
+            break
+    return br
+
+def BR2(a):
+    br = None
+    for i in range(BR(a)):
+        p0 = a[len(a)-1-i:len(a)]
+        p1 = a[BR(a):len(a)]
+        if x(p0,p1):
             br=len(a)-i
             break
     return br
